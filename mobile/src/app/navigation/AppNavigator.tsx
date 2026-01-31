c5702e431b702ed4d87aa66b6658adccc6b0a2d4
elieisenstein <elieisenstein@gmail.com>
2026-02-01 01:39:08 +0200

    version 1.0.7

--- mobile/src/app/navigation/AppNavigator.tsx
+++ mobile/src/app/navigation/AppNavigator.tsx
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useTranslation } from "react-i18next";
 import { Icon } from "react-native-paper";
 import { getLastTab, setLastTab, TabName } from "../../lib/preferences";
+import { checkForUpdate } from "../../lib/versionCheck";
 
 import FeedScreen from "../screens/FeedScreen";
 import ProfileScreen from "../screens/ProfileScreen";
@@ -218,6 +219,8 @@ export default function AppNavigator() {
 
   useEffect(() => {
     getLastTab().then(setInitialTab);
+    // Check for app updates on launch
+    checkForUpdate();
   }, []);
 
   if (!initialTab) {
